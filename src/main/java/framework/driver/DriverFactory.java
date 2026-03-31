362eb9b8de20b6f07a839c9856f59f068e820934
Khanh Ngoc <khanhngoc@gmail.com>
2026-03-31 14:49:03 +0700

    Add matrix strategy for Chrome and Firefox

--- src/main/java/framework/driver/DriverFactory.java
+++ src/main/java/framework/driver/DriverFactory.java
@@ -2,31 +2,43 @@ package framework.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
     public static WebDriver createDriver(String browser) {
-        if (browser == null) {
+        if (browser == null || browser.isBlank()) {
             browser = "chrome";
         }
+
+        boolean isCI = System.getenv("CI") != null;
 
         switch (browser.toLowerCase()) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
-                return new FirefoxDriver();
-
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                return new EdgeDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (isCI) {
+                    firefoxOptions.addArguments("-headless");
+                }
+                return new FirefoxDriver(firefoxOptions);
 
             case "chrome":
             default:
                 WebDriverManager.chromedriver().setup();
-                return new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (isCI) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                } else {
+                    chromeOptions.addArguments("--start-maximized");
+                }
+                return new ChromeDriver(chromeOptions);
         }
     }
 }
